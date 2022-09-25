40be41c558c47cfce9127924553fedf8ea1b2fb2
Jordi <jordi8101@gmail.com>
2022-09-25 16:15:57 -0700

    Added rooms

---
+++ server/utils/rooms.js
@@ -0,0 +1,35 @@
+import { isEmpty } from "./nativeMethods";
+const { getCache, setCache, getKeys, delCache } = require("./redis");
+
+async function createRoom(data) {
+  await setCache(`room-${data.room}`, {
+    room: data.room,
+  });
+  return await getCache(`room-${data.room}`);
+}
+async function getRoom(room) {
+  const data = await getCache(`room-${room}`);
+  return data;
+}
+async function getRooms() {
+  const rooms = await getKeys("room-*");
+  const roomsData = [];
+  if (rooms) {
+    for (let i = 0; i < rooms.length; i++) {
+      const room = await getCache(rooms[i]);
+      roomsData.push(room);
+    }
+  }
+  return roomsData;
+}
+async function deleteRoom(room) {
+  await delCache(`room-${room}`);
+}
+
+module.exports = {
+  createRoom,
+  getRoom,
+  getRooms,
+  updateRoomStatus,
+  deleteRoom,
+};
